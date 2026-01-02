735a6daa141c74bd2776a3896188b0944b9407e0
clapbin <clapbin78@gmail.com>
2026-01-02 15:31:23 +0900

    initial commit

---
+++ RxSwiftCleanArchitectureMVVMTests/RxSwiftCleanArchitectureMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwiftCleanArchitectureMVVMTests.swift
+//  RxSwiftCleanArchitectureMVVMTests
+//
+//  Created by clapbin on 1/2/26.
+//
+
+import Testing
+@testable import RxSwiftCleanArchitectureMVVM
+
+struct RxSwiftCleanArchitectureMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
